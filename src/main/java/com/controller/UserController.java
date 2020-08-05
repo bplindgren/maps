b86295f456baadb44b15eecf6b959fb866d69372
bplindgren <bplindgren91@gmail.com>
2020-08-04 22:39:38 -0500

    create user_authorities at user creation

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.controller;
 
 import java.net.URI;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +26,18 @@ public class UserController {
 	public UserController(UserService userService, UserRepository userRepository) {
 		this.userService = userService;
 		this.userRepository = userRepository;
+	}
+	
+	@GetMapping("/users/{username}")
+	public ResponseEntity<UserDTO> getUser(@PathVariable String username) throws Exception{
+		Optional<User> user = userService.findByUsername(username);
+		if(user.isPresent()) {
+			UserDTO userDTO = new UserDTO(user.get());
+			ResponseEntity<UserDTO> response = new ResponseEntity<UserDTO>(userDTO, HttpStatus.OK);
+			return response;
+		} else {
+			throw new Exception("User was not found");
+		}
 	}
 	
 	/**
